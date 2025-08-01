46f1b5e482cbcf4c42d5dcc93a4282d01652657f
Saexy <thekoala250303@gmail.com>
2025-08-01 20:17:42 -0300

    fix: adjust ssl typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -15,6 +15,7 @@ export const dataSource = new DataSource({
   database: process.env.DATABASE_NAME,
   entities: [DocumentType, Employee, Document],
   migrations: [__dirname + '/../migrations/*.ts'],
+  ssl: (process.env.PORT ?? 3000 === 3000) ? false : true,
   synchronize: false,
   migrationsRun: true,
 });

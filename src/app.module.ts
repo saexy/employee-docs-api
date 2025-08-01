4479b3f429876f35159d61026597406eaa59465e
Saexy <thekoala250303@gmail.com>
2025-08-01 20:48:42 -0300

    feat: adding rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentTypeModule } from './modules/document-type/document-type.module';
 import { EmployeeModule } from './modules/employee/employee.module';
 import { DocumentModule } from './modules/document/document.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
     DocumentTypeModule,
     EmployeeModule,
     DocumentModule,
